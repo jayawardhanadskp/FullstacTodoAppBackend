39616afed0ffce226c87d85eaa428f133bcd5754
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-09-18 09:41:45 +0530

    handle api status codes

--- controller/todo.controller.js
+++ controller/todo.controller.js
@@ -4,11 +4,16 @@ exports.createToDo = async (req, res, next) => {
     try {
         const {userId, title, desc} = req.body;
 
+        if (!userId || !title || !desc) {
+            return res.status(400).json({status: false, message: 'All fields re require'})
+        }
+
         let todo = await ToDoServices.createToDo(userId, title, desc);
 
-        res.json({status:true, success:todo});
+        res.status(201).json({status:true, success:todo});
     } catch (e) {
         next(e);
+        res.status(500).json({status: false, message: 'Fail to create todo', error: e.message});
     }
 }
 
@@ -16,10 +21,19 @@ exports.getUserToDo = async (req, res, next) => {
     try {
         const {userId} = req.body;
 
+        if (!userId) {
+            return res.status(400).json({status: false, message : 'User Id is required'});
+        }
+
         let todo = await ToDoServices.getToDodata(userId);
+
+        if (!todo) {
+            return res.status(404).json({ status: false, message: 'No Data'});
+        }
 
         res.json({status: true, success:todo});
     } catch (e) {
         next(e);
+        res.status(500).json({status: false, message:  'Fail to get user todo', error: e.message});
     }
 }
